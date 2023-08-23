7d21d15b5ad940611fb37a0c8b5cca57bb504d16
Dinorah <dinorah.garciavasquez@gmail.com>
2023-08-23 19:30:25 -0400

    Zombies

---
+++ zombie.js
@@ -0,0 +1,55 @@
+import * as PIXI from "pixi.js";
+import Victor from "victor";
+
+export default class Zombie {
+    constructor({app, player}){
+    this.app = app;
+    this.player = player;
+
+    const Radius = 16;
+    this.Speed = 2;
+    this.zombie = new PIXI.Graphics();
+    let r = this.randomSpawnPoint();
+    this.zombie.position.set(r.x,r.y);
+    this.zombie.beginFill(0xFF0000, 1);
+    this.zombie.drawCircle(0,0, Radius);
+    this.zombie.endFill();
+    app.stage.addChild(this.zombie);
+    }
+    update(){ 
+    let e = new Victor(this.zombie.position.x, this.zombie.position.y);
+    let s = new Victor(this.player.position.x, this.player.position.y);
+
+    if (e.distance(s) < this.player.width / 2){
+      let r = this.randomSpawnPoint();
+      this.zombie.position.set(r.x, r.y);
+      return;
+    }
+    let d = s.subtract(e);
+    let v = d.normalize().multiplyScalar(this.Speed);
+    this.zombie.position.set(this.zombie.position.x + v.x,this.zombie.position.y + v.y)
+    }
+    randomSpawnPoint() {
+        let edge = Math.floor(Math.random() * 4); //random int between 0-3
+        let spawnPoint = new Victor(0,0);
+        let canvasSize = this.app.screen.width;
+        switch (edge) {
+          case 0: //top
+          spawnPoint.x = canvasSize * Math.random();
+          break;
+          case 1: //right
+          spawnPoint.x = canvasSize;
+          spawnPoint.y = canvasSize * Math.random();
+          break;
+          case 2: //bottom
+          spawnPoint.x = canvasSize * Math.random();
+          spawnPoint.y = canvasSize;
+          break;
+          default: //left
+          spawnPoint.x = 0;
+          spawnPoint.y = canvasSize * Math.random();
+          break;
+        }
+        return spawnPoint
+      }
+}

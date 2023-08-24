6391d6b50720d0e16710474b0b9ef81054db1389
Dinorah Garcia Vasquez <83092931+DinorahGV02@users.noreply.github.com>
2023-08-24 18:13:38 -0400

    Movement Smooth

--- controller.js
+++ controller.js
@@ -1,36 +1,60 @@
 import * as PIXI from "pixi.js";
 import Victor from "victor";
 
-export default class Controller{
-    constructor({app, player}){
-        this.app = app
-        this.player = player
+export default class Controller {
+    constructor({ app, player }) {
+        this.app = app;
+        this.player = player;
+        this.isPaused = false;
+
+        // Initialize keysPressed as an empty object
+        this.keysPressed = {};
+
+        this.setupInputListeners(); // Call a separate method to set up input listeners
     }
-    
-    update(delta){
-        const up = new Victor(0,-1);
-        const down = new Victor(0,1);
-        const left = new Victor(-1,0);
-        const right =new Victor(1,0);
-        let speed = 0.1;
-        
-        window.addEventListener("keydown", (e)=>{
-            if(e.code === 'KeyW'){
-                this.player.position.set(this.player.position.x, this.player.position.y + up.y * speed * delta);
+
+    setupInputListeners() {
+        window.addEventListener("keydown", (e) => {
+            if (e.code === 'KeyW' || e.code === 'KeyS' || e.code === 'KeyA' || e.code === 'KeyD') {
+                this.isPaused = false; // Resume updates on key press
+                this.keysPressed[e.code] = true; // Set the corresponding key as pressed
             }
-            if(e.code === 'KeyS'){
-                this.player.position.set(this.player.position.x, this.player.position.y + down.y * speed * delta);
-            }
-            if(e.code === 'KeyA'){
-                this.player.position.set(this.player.position.x+ left.x * speed * delta, this.player.position.y);
-            }
-            if(e.code === 'KeyD'){
-                this.player.position.set(this.player.position.x+ right.x * speed * delta, this.player.position.y);
-            }
-        })
+        });
 
         window.addEventListener("keyup", (e) => {
-            delta = 0;
-        })
+            this.isPaused = true;
+            this.keysPressed[e.code] = false; // Set the corresponding key as released
+        });
+    }
+
+    update(delta) {
+        if (this.isPaused) {
+            return; // Do not update if paused
+        }
+
+        const up = new Victor(0, -1);
+        const down = new Victor(0, 1);
+        const left = new Victor(-1, 0);
+        const right = new Victor(1, 0);
+        const speed = 2;
+
+        // Calculate new position based on keys pressed
+        let newPosition = new Victor(this.player.position.x, this.player.position.y);
+
+        if (this.keysPressed['KeyW']) {
+            newPosition.add(up.clone().multiplyScalar(speed * delta));
+        }
+        if (this.keysPressed['KeyS']) {
+            newPosition.add(down.clone().multiplyScalar(speed * delta));
+        }
+        if (this.keysPressed['KeyA']) {
+            newPosition.add(left.clone().multiplyScalar(speed * delta));
+        }
+        if (this.keysPressed['KeyD']) {
+            newPosition.add(right.clone().multiplyScalar(speed * delta));
+        }
+
+        // Update player position
+        this.player.position.set(newPosition.x, newPosition.y);
     }
 }
